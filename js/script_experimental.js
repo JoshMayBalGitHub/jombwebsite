f8764284c5fea9d078663c59b7fd48252f812526
JoshMayBalGitHub <93077235+JoshMayBalGitHub@users.noreply.github.com>
2022-03-15 18:11:39 +0800

    added back the experimental page part 2

---
+++ js/script_experimental.js
@@ -0,0 +1,23 @@
+
+// the function "CD" makes the manual-button timer work. 
+function CD() {
+    var CV = document.getElementById("CDB").innerHTML
+    var NV = 0;
+    if (CV > 0) {
+        NV = CV - 1;
+    }
+    document.getElementById("CDB").innerHTML = NV;
+}
+// Oh btw the function AG is not used
+function AG() {
+    var NT = "LOL"
+    document.getElementById("ABS").innerHTML = NT;
+}
+
+function J1() {
+    window.location.href = "experiment.html";
+}
+
+function RM() {
+    document.getElementById("HM").style.display = "block"
+}
