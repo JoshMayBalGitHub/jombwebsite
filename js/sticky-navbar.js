d97b5757bfa629642eefd0e55e6cb6f8fdb81548
JoshMayBal <joshuamiguelcalara@gmail.com>
2023-04-23 22:37:54 +0800

    intro part 2 DONE
    damn this took 3 days to do, for one page, this seems lazy but whatever

---
+++ js/sticky-navbar.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {stickyNavbar()};
+  
+var navbar = document.getElementById("sticky-navbar-tb");
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky-navbar")
+  } else {
+    navbar.classList.remove("sticky-navbar");
+  }
+}
