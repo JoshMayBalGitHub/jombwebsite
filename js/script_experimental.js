918a877d6cbe521adf95214acd10e68f43d06442
JoshMayBalGitHub <joshuamiguelcalara@gmail.com>
2023-06-18 01:07:29 +0800

    add "stories"
    now a "writer", if you read them you will know...

--- js/script_experimental.js
+++ js/script_experimental.js
@@ -10,9 +10,16 @@ function MouseClickTest() {
 function BPJ() {
     window.location.href = "index.html";
 }
+// TO INDEX BUTTONS todo: pls fix
 function gohome() {
     window.location.href = "../index.html";
 }
+
+function gohome_more() {
+    window.location.href = "../../index.html";
+}
+// TO INDEX BUTTONS todo: pls fix
+
 /*Kinda wish this is not what i have to do every darn time i have to link stuff* - fixed 21 May 2023*/
 function Unhide1() {
     document.getElementById("HM").style.display = "block"
